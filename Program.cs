8dc5794bad0856c5e2276d2afc980217f4996a51
Yoel Hor <yoelhor@live.com>
2023-07-09 10:56:09 +0300

    Nuget packages

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages()
-    .AddMicrosoftIdentityUI();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
