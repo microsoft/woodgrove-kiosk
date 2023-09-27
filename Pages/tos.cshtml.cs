0171c54a9b4c9e92c33f7288f7afd7d098d4b2b2
Yoel Horvitz <yoelhor@live.com>
2023-09-27 16:29:51 +0300

    Privacy and TOS

---
+++ Pages/tos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace woodgrovedemo_kiosk.Namespace
+{
+    public class tosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
